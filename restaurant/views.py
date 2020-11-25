1df6499c71b35b9f667de61a879b43f390d23a44
Nicholas De La Cruz <njdlc22@gmail.com>
2020-11-25 17:13:28 -0500

    Add more templates

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Douglas Troeger',
+        'title': 'Exam 2 Comments',
+        'content': 'Trash exams. Drop the course.',
+        'date_posted': 'November 25, 2020'
+    },
+    {
+        'author': 'Douglas Troeger',
+        'title': 'Update to Project',
+        'content': 'Project is cancelled. You all get 0s.',
+        'date_posted': 'November 25, 2020'
+    }
+]
+
 # Create your views here.
 def index(request):
     return HttpResponse('Welcome to the restaurant')
+    #return render(request, './About.html')
+
+def about(request):
+    return render(request, 'restaurant/about.html')
+    #return HttpResponse('About Page')
+
+def complaint_compliment(request):
+    return render(request, 'restaurant/complaint_compliment.html')
+
+def deposit(request):
+    # user's current balance goes here
+    return render(request, 'restaurant/deposit.html')
+
+def discussion_board(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'restaurant/discussion_board.html', context)
+
+def home(request):
+    return render(request, 'restaurant/home.html')
+
+def menu(request):
+    # user's personalized dishes go here
+    # menu goes here
+    return render(request, 'restaurant/menu.html')
+
+def register(request):
+    return render(request, 'restaurant/register.html')
